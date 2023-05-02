69441c97773ac70cec54c1856ca4c63b8883801b
rohilpatel15 <rohilpatel123@outlook.com>
2023-05-01 19:17:10 -0700

    log-in, sign up, log out all work with the members page

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const session = require('express-session');
 const Mongostore = require('connect-mongo');
 const bcrypt = require('bcrypt');
 
+const saltRounds = 12;
+
 const Joi = require('joi');
 
 const app = express();
@@ -42,8 +44,28 @@ app.use(session({
 ));
 
 app.get('/', (req, res) => {
-    res.send("<h1>Hello World</h1>");
+    var html = '';
+    if (req.session.username) {
+        html += `
+            <h1>Hello, ${req.session.username}!</h1>
+            <p>You are currently logged in.</p>
+            <p><a href="/members">Go to members page</a></p>
+            <form action="/logout" method="post">
+                <button type="submit">Log Out</button>
+            </form>
+        `;
+    } else {
+        html += `
+            <h1>Welcome to My Site</h1>
+            <p>Please sign up or log in:</p>
+                <a href="/createUser">Sign up</a>
+                <a href="/login">Log in</a>
+        `;
+    }
+    res.send(html);
 });
+
+
 
 app.get('/nosql-injection', async (req,res) => {
 	var username = req.query.user;
@@ -106,6 +128,7 @@ app.post('/submitUser', async (req,res) => {
 		});
 	
 	const validationResult = schema.validate({username, password});
+
 	if (validationResult.error != null) {
 	   console.log(validationResult.error);
 	   res.redirect("/createUser");
@@ -117,9 +140,25 @@ app.post('/submitUser', async (req,res) => {
 	await userCollection.insertOne({username: username, password: hashedPassword});
 	console.log("Inserted user");
 
-    var html = "successfully created user";
+    req.session.username = username;
+
+    res.redirect('/members');
+});
+
+app.get('/members', (req, res) => {
+    if (!req.session.username) {
+        res.redirect('/login');
+    }
+    var html = `
+        <h1>Welcome, ${req.session.username}!</h1>
+        <p>You are now a member of our site.</p>
+		<form action="/logout" method="post">
+            <button type="submit">Log Out</button>
+        </form>
+    `;
     res.send(html);
 });
+
 
 app.post('/loggingin', async (req,res) => {
     var username = req.body.username;
@@ -127,6 +166,7 @@ app.post('/loggingin', async (req,res) => {
 
 	const schema = Joi.string().max(20).required();
 	const validationResult = schema.validate(username);
+
 	if (validationResult.error != null) {
 	   console.log(validationResult.error);
 	   res.redirect("/login");
@@ -135,19 +175,18 @@ app.post('/loggingin', async (req,res) => {
 
 	const result = await userCollection.find({username: username}).project({username: 1, password: 1, _id: 1}).toArray();
 
-	console.log(result);
 	if (result.length != 1) {
 		console.log("user not found");
 		res.redirect("/login");
 		return;
 	}
+
 	if (await bcrypt.compare(password, result[0].password)) {
 		console.log("correct password");
-		req.session.authenticated = true;
-		req.session.username = username;
-		req.session.cookie.maxAge = expireTime;
 
-		res.redirect('/loggedIn');
+		req.session.username = username;
+
+		res.redirect('/members');
 		return;
 	}
 	else {
@@ -167,14 +206,42 @@ app.get('/loggedin', (req,res) => {
     res.send(html);
 });
 
-app.get('/logout', (req,res) => {
-	req.session.destroy();
-    var html = `
-    You are logged out.
-    `;
-    res.send(html);
+app.post('/logout', (req, res) => {
+    req.session.destroy(() => {
+        mongoStore.destroy(req.sessionID, (err) => {
+            if (err) {
+                console.log(err);
+            }
+            res.redirect('/');
+        });
+    });
 });
 
-app.listen(port, () => {
-    console.log(`Node is running on port ${port}`);
+
+
+app.get('/cat/:id', (req,res) => {
+
+    var cat = req.params.id;
+
+    if (cat == 1) {
+        res.send("Fluffy: <img src='/fluffy.gif' style='width:250px;'>");
+    }
+    else if (cat == 2) {
+        res.send("Socks: <img src='/socks.gif' style='width:250px;'>");
+    }
+    else {
+        res.send("Invalid cat id: "+cat);
+    }
 });
+
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("*", (req,res) => {
+	res.status(404);
+	res.send("Page not found - 404");
+})
+
+app.listen(port, () => {
+	console.log("Node application listening on port "+port);
+}); 
